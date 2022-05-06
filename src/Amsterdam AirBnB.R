5dcfff36ef7e3f40bb6398cd5228991d48158576
unknown <i.r.b.heuten@tilburguniversity.edu>
2022-05-06 11:12:53 +0200

    Update with regression

--- src/Amsterdam AirBnB.R
+++ src/Amsterdam AirBnB.R
@@ -312,3 +312,39 @@ Price_Change <- print(Difference_colMeans/colMeans_See_All_neighbourhoods_withou
 
 Price_Change_100 <- print(Price_Change * 100)
 
+####################################################################
+#create new dataset with the neighborhood, price and review scores##
+####################################################################
+listings2.0 <-  subset(listings, select = c ("neighbourhood_cleansed", "price", "review_scores_rating")) %>%
+    filter(na.rm = TRUE)
+
+############################################################################################################
+#conduct a linear regression to examine the relationship between price as dv and review_scores_rating as iv#
+############################################################################################################
+listings_lm1 <- lm(price~review_scores_rating, AirBNB_AMS_listings);
+summary(listings_lm1)
+        
+#create a table with the average review score per neighborhood
+listings <- AirBNB_AMS_listings %>%
+    select(neighbourhood_cleansed, price, review_scores_rating)
+
+
+Table_reviewscore <-  (listings%>%
+                           group_by(neighbourhood_cleansed) %>%
+                           na.omit(listings) %>%
+                           summarise_at(vars(review_scores_rating), list(review_scores_rating = mean)))
+        
+print(Table_reviewscore, n=Inf, na.rm=TRUE)
+
+#plot to see whether there is normality
+ggplot(AirBNB_AMS_listings, aes(review_scores_rating))+ geom_histogram(bins=50)
+        
+        
+#plot to see whether there is linearity
+ggplot(AirBNB_AMS_listings, aes(x=review_scores_rating, y=price)) + geom_point()
+        
+#plot to check for homoscedasticity
+review_hmsc <- AirBNB_AMS_listings$price
+ggplot(AirBNB_AMS_listings, aes(x=review_scores_rating, y=review_hmsc))+ geom_point()
+        
+        
